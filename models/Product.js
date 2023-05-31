6809826b131229ab5513595018efa3dbfea5c844
Joao Marchete <elisamarchete@gmail.com>
2023-05-31 14:38:21 -0400

    Associations part

--- models/Product.js
+++ models/Product.js
@@ -37,8 +37,15 @@ Product.init(
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: "Category", // the name of the referenced model
+        model: "category", // the name of the referenced model
         key: "id", // the column in the referenced model to link with
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
       },
     },
   },
